1d28df080a203ac1354ea1171d9560da614f3dc5
Shravxnk <shravankadam2005@gmail.com>
2024-04-13 22:59:50 +0530

    init

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('my-cache').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                'CSS/style.css',
+                'CSS/animate.css',
+                '/main.js',
+                'dist/build.js',
+                '/images/logo.png',
+                '/manifest.json',
+                '/package.json',
+                '/package-lock.json',
+                '/service-worker.js',
+                // Add any other assets you want to cache
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
